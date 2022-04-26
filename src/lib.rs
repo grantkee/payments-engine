ed1dfb2accb9fb5815d5332ab29ba87aed9bc662
grantkee <grant@ether.ai>
2022-04-26 16:11:59 -0500

    added client mod and impl new(). refactored lib.rs to handle application logic as an 'Engine' struct with clients and transactions. unit tests for default values when making engine struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,40 @@
 pub use error::Error;
+pub use std::{path, collections};
 pub use transaction::TransactionInfo;
-pub use std::path;
+pub use client::Client;
 
 mod error;
 mod transaction;
+mod client;
 
-pub async fn run(path: path::PathBuf) -> Result<(), Error>{
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .trim(csv::Trim::All)
-        .from_path(path)?;
-
-    for result in rdr.deserialize() {
-        let record: TransactionInfo = result?;
-        println!("{:?}", record);
-    }
-
-    Ok(())
+#[derive(Debug, Default)]
+pub struct Engine {
+    // consider using BTreeMap if sorting by ids
+    pub transactions: collections::HashMap<u32, TransactionInfo>,
+    pub clients: collections::HashMap<u16, Client>
 }
 
+impl Engine {
+    /// Read the CSV file from the argument provided to CLI.
+    pub async fn read(&self, path: path::PathBuf) -> Result<(), Error> {
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .trim(csv::Trim::All)
+            .from_path(path)?;
+    
+        for result in rdr.deserialize() {
+            let record: TransactionInfo = result?;
+            println!("{:?}", record);
+        }
+    
+        Ok(())
+    }
+
+    /// Write to stdout
+    pub async fn write(&self) -> Result<(), Error> {
+        unimplemented!();
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -26,13 +42,22 @@ mod tests {
 
     #[tokio::test]
     async fn can_handle_whitespace() {
-        let result = run(path::PathBuf::from("./tests/CSVs/transactions.csv"));
-        assert!(result.await.is_ok())
+        let engine = Engine::default();
+        let result = engine.read(path::PathBuf::from("./tests/CSVs/transactions.csv")).await;
+        assert!(result.is_ok())
     }
 
     #[tokio::test]
     async fn correct_path_must_exist() {
-        let result = run(path::PathBuf::from("./tests/NONEXISTENT_PATH.csv"));
-        assert!(result.await.is_err())
+        let engine = Engine::default();
+        let result = engine.read(path::PathBuf::from("./tests/NONEXISTENT_PATH.csv")).await;
+        assert!(result.is_err())
+    }
+
+    #[tokio::test]
+    async fn new_engine_defaults() {
+        let engine = Engine::default();
+        assert!(engine.transactions.is_empty());
+        assert!(engine.clients.is_empty());
     }
 }
