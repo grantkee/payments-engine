3d6c851490189b75cf106b97f0ebc974d47359cb
grantkee <grant@kee.ai>
2022-04-26 16:58:32 -0500

    structs for client

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,95 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+/// Unique struct.
+///
+/// Struct for writing to CSV.
+#[derive(Debug)]
 pub struct Client {
+    pub id: u16,
+    pub available: f64,
+    pub held: f64,
+    pub total: f64,
+    pub locked: bool,
+}
+
+/// Unique struct.
+/// 
+/// Minimum amount of information needed for
+/// managing client's transactions.
+#[derive(Debug, Deserialize)]
+pub struct ClientInfo {
     id: u16,
-    available: f64,
-    held: f64,
-    total: f64,
+    funds: ClientFunds,
     locked: bool,
 }
 
-impl Client {
+/// Struct for handling client's funds.
+#[derive(Debug, Default, Deserialize)]
+struct ClientFunds {
+    // total = available + held
+    available: f64,
+    held: f64
+}
+
+impl ClientInfo {
+    /// Create unique client with minimum amount
+    /// of information needed to process all 
+    /// transactions.
     pub async fn new(id: u16) -> Self {
         Self {
             id,
-            available: 0.0,
-            held: 0.0,
-            total: 0.0,
+            funds: ClientFunds::default(),
             locked: false,
         }
+    }
+
+    // only used in trait impl From<ClientInfo>
+    // not async to prevent heal allocation
+    /// Calculate total for Client based on 
+    /// available and held funds.
+    pub fn get_total(&self) -> f64 {
+        self.funds.held + self.funds.available
+    }
+}
+
+
+/// Create the Client struct from ClientInfo.
+/// Used to write back to CSV.
+impl From<ClientInfo> for Client {
+    // do not want to use async-trait because
+    // it results in heap allocation per-function-call
+    fn from(info: ClientInfo) -> Self {
+        Self {
+            id: info.id,
+            available: info.funds.available,
+            held: info.funds.held,
+            total: info.get_total(),
+            locked: info.locked,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn new_client_defaults() {
+        let client = ClientInfo::new(1).await;
+        assert_eq!(client.id, 1);
+        assert_eq!(client.funds.available, 0.0);
+        assert_eq!(client.funds.held, 0.0);
+        assert_eq!(client.locked, false);
+    }
+
+    #[tokio::test]
+    async fn create_client_from_client_info() {
+        let client_info = ClientInfo::new(1).await;
+        let client = Client::from(client_info);
+        assert_eq!(client.id, 1);
+        assert_eq!(client.available, 0.0);
+        assert_eq!(client.held, 0.0);
+        assert_eq!(client.total, 0.0);
+        assert_eq!(client.locked, false);
     }
 }
