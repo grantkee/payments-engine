b2a77b9c29b67d8477eb18c4e6d533112cdf67b3
grantkee <grant@ether.ai>
2022-04-26 12:43:07 -0500

    basic structure for main.rs - async with structopt. added anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use std::path;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    path: path::PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+    let file = Opt::from_args();
+    println!("file: {:?}", file.path);
+    Ok(())
 }
