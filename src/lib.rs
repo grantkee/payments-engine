ee997cd6143badf7bb3ec799c791273400fb5406
grantkee <grant@ether.ai>
2022-04-26 18:53:25 -0500

    matching transaction types from engine.read(). added two new error types and more tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,62 @@
+pub use client::{Client, ClientInfo};
 pub use error::Error;
-pub use std::{path, collections};
+pub use std::{collections::HashMap, path};
 pub use transaction::TransactionInfo;
-pub use client::Client;
 
+mod client;
 mod error;
 mod transaction;
-mod client;
 
 #[derive(Debug, Default)]
 pub struct Engine {
     // consider using BTreeMap if sorting by ids
-    pub transactions: collections::HashMap<u32, TransactionInfo>,
-    pub clients: collections::HashMap<u16, Client>
+    pub transactions: HashMap<u32, TransactionInfo>,
+    pub clients: HashMap<u16, ClientInfo>,
 }
 
 impl Engine {
     /// Read the CSV file from the argument provided to CLI.
-    pub async fn read(&self, path: path::PathBuf) -> Result<(), Error> {
+    pub async fn read(&mut self, path: path::PathBuf) -> Result<(), Error> {
         let mut rdr = csv::ReaderBuilder::new()
             .has_headers(true)
             .trim(csv::Trim::All)
             .from_path(path)?;
-    
+
         for result in rdr.deserialize() {
-            let record: TransactionInfo = result?;
-            println!("{:?}", record);
+            let transaction: TransactionInfo = result?;
+            println!("{:?}", transaction);
+
+            // let mut client = self
+            //     .clients
+            //     .entry(transaction.client)
+            //     .or_insert_with(|| Box::pin(ClientInfo::new(transaction.client).await));
+
+            // TODO: implement async
+            let mut client = self
+                .clients
+                .entry(transaction.client)
+                .or_insert_with_key(|_| ClientInfo::new(transaction.client));
+
+            if self.transactions.contains_key(&transaction.tx) {
+                // do not process transactions more than once
+                println!("Transaction {:?} already exists", transaction.tx);
+                return Err(Error::TransactionAlreadyProcessed(transaction.tx));
+            } else {
+                match transaction.r#type.as_str() {
+                    "deposit" => println!("transaction is deposit"),
+                    "withdrawal" => println!("transaction is withdrawal"),
+                    "dispute" => println!("transaction is a dispute"),
+                    "resolve" => println!("transaction is a resolve"),
+                    "chargeback" => println!("transaction is a chargeback"),
+                    _ => return Err(Error::UnknownTransactionType(transaction.r#type)),
+                }
+            }
         }
-    
+
+        for (k, v) in &self.clients {
+            println!("{:?} - {:?}", k, v);
+        }
+
         Ok(())
     }
 
@@ -42,22 +72,44 @@ mod tests {
 
     #[tokio::test]
     async fn can_handle_whitespace() {
-        let engine = Engine::default();
-        let result = engine.read(path::PathBuf::from("./tests/CSVs/transactions.csv")).await;
+        let mut engine = Engine::default();
+        let result = engine
+            .read(path::PathBuf::from("./tests/CSVs/whitespace.csv"))
+            .await;
         assert!(result.is_ok())
     }
 
     #[tokio::test]
     async fn correct_path_must_exist() {
-        let engine = Engine::default();
-        let result = engine.read(path::PathBuf::from("./tests/NONEXISTENT_PATH.csv")).await;
+        let mut engine = Engine::default();
+        let result = engine
+            .read(path::PathBuf::from("./tests/NONEXISTENT_PATH.csv"))
+            .await;
         assert!(result.is_err())
     }
 
     #[tokio::test]
     async fn new_engine_defaults() {
-        let engine = Engine::default();
+        let mut engine = Engine::default();
         assert!(engine.transactions.is_empty());
         assert!(engine.clients.is_empty());
+    }
+
+    #[tokio::test]
+    async fn unknown_transaction_type_fails() {
+        let mut engine = Engine::default();
+        let result = engine
+            .read(path::PathBuf::from("./tests/unknown_transaction_type.csv"))
+            .await;
+        assert!(result.is_err())
+    }
+
+    #[tokio::test]
+    async fn duplicate_transactions_fail() {
+        let mut engine = Engine::default();
+        let result = engine
+            .read(path::PathBuf::from("./tests/duplicate_transactions.csv"))
+            .await;
+        assert!(result.is_err())
     }
 }
