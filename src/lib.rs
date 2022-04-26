d87aa6aeb446641068869be765b8993e15e5cc75
grantkee <grant@kee.ai>
2022-04-26 14:39:06 -0500

    csv reader handling whitespace - unit tests started

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,31 @@ mod transaction;
 pub async fn run(path: path::PathBuf) -> Result<(), Error>{
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(true)
+        .trim(csv::Trim::All)
         .from_path(path)?;
 
     for result in rdr.deserialize() {
         let record: TransactionInfo = result?;
         println!("{:?}", record);
     }
+
     Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn can_handle_whitespace() {
+        let result = run(path::PathBuf::from("./tests/CSVs/transactions.csv"));
+        assert!(result.await.is_ok())
+    }
+
+    #[tokio::test]
+    async fn correct_path_must_exist() {
+        let result = run(path::PathBuf::from("./tests/NONEXISTENT_PATH.csv"));
+        assert!(result.await.is_err())
+    }
 }
